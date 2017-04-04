6db517eea2969eb2a878948815cf914f6fa654cf
Gary Burgess <gary.burgess@gmail.com>
2017-04-04 01:51:23 +0100

    Add a generator for `NonEmpty` values

--- src/Control/Monad/Gen/Common.purs
+++ src/Control/Monad/Gen/Common.purs
@@ -3,12 +3,15 @@ module Control.Monad.Gen.Common where
 import Prelude
 
 import Control.Apply (lift2)
-import Control.Monad.Gen (class MonadGen, choose, chooseFloat)
+import Control.Monad.Gen (class MonadGen, resize, choose, chooseFloat, unfoldable)
+import Control.Monad.Rec.Class (class MonadRec)
 
 import Data.Either (Either(..))
 import Data.Identity (Identity(..))
 import Data.Maybe (Maybe(..))
+import Data.NonEmpty (NonEmpty, (:|))
 import Data.Tuple (Tuple(..))
+import Data.Unfoldable (class Unfoldable)
 
 -- | Creates a generator that outputs `Either` values, choosing a value from a
 -- | `Left` or the `Right` with even probability.
@@ -34,3 +37,18 @@ genMaybe gen = do
 -- | pair of generators for each slot in the tuple.
 genTuple :: forall m a b. Apply m => m a -> m b -> m (Tuple a b)
 genTuple = lift2 Tuple
+
+-- | Creates a generator that outputs `NonEmpty` values, choosing values from a
+-- | generator for each of the items.
+-- |
+-- | The size of the value will be determined by the current size state
+-- | for the generator. To generate a value of a particular size, use the
+-- | `resize` function from the `MonadGen` class first.
+genNonEmpty
+  :: forall m a f
+   . MonadRec m
+  => MonadGen m
+  => Unfoldable f
+  => m a
+  -> m (NonEmpty f a)
+genNonEmpty gen = (:|) <$> gen <*> resize (_ - 1) (unfoldable gen)
