943279546623971a063ed906d302c54362ab5f40
Gary Burgess <gary.burgess@gmail.com>
2018-04-17 01:19:07 +0100

    Updates for 0.12

--- test/Test/Frequency.purs
+++ test/Test/Frequency.purs
@@ -2,18 +2,17 @@ module Test.Frequency where
 
 import Prelude
 
-import Test.Assert (ASSERT, assert)
-import Partial.Unsafe (unsafeCrashWith)
-import Control.Monad.Eff (Eff)
 import Control.Monad.Gen (class MonadGen, frequency)
+import Control.Monad.State (State, class MonadState, get, put, evalStateT)
+import Data.Array (replicate, group', length)
+import Data.Newtype (unwrap)
 import Data.NonEmpty ((:|), NonEmpty(..))
 import Data.Traversable (sequence)
-import Data.Array (replicate, group', length)
 import Data.Tuple (Tuple(..))
-import Data.Newtype (unwrap)
+import Effect (Effect)
 import Math (remainder)
-import Control.Monad.State (State, class MonadState, get, put, evalStateT)
-
+import Partial.Unsafe (unsafeCrashWith)
+import Test.Assert (assert)
 
 newtype TestGenFrequency a = TestGenFrequency (State Number a)
 derive newtype instance testGenFunctor :: Functor TestGenFrequency
@@ -27,7 +26,7 @@ instance testGenMonadGen :: MonadGen TestGenFrequency where
   sized _ = unsafeCrashWith "sized should not be called"
   resize _ _ = unsafeCrashWith "resize should not be called"
   chooseBool = pure unit >>= \_ -> unsafeCrashWith "chooseBool should not be called"
-  chooseFloat s e = do 
+  chooseFloat s e = do
     c <- get
     put (c + 1.0)
     pure ((s + c) `remainder` e)
@@ -36,24 +35,24 @@ instance testGenMonadGen :: MonadGen TestGenFrequency where
 runTestGenFrequency :: TestGenFrequency ~> State Number
 runTestGenFrequency (TestGenFrequency x) = x
 
-check :: forall r. Eff (assert :: ASSERT | r) Unit
-check = 
+check :: Effect Unit
+check =
   let
     abcGen :: TestGenFrequency String
-    abcGen = 
-      frequency $ 
-        ( Tuple 10.0 $ pure "A" ) :| 
-        [ Tuple 20.0 $ pure "B" 
-        , Tuple 0.0 $ pure "Z" 
-        , Tuple 30.0 $ pure "C" 
-        , Tuple 40.0 $ pure "D" 
-        , Tuple 50.0 $ pure "E" 
-        , Tuple 50.0 $ pure "F" 
+    abcGen =
+      frequency $
+        ( Tuple 10.0 $ pure "A" ) :|
+        [ Tuple 20.0 $ pure "B"
+        , Tuple 0.0 $ pure "Z"
+        , Tuple 30.0 $ pure "C"
+        , Tuple 40.0 $ pure "D"
+        , Tuple 50.0 $ pure "E"
+        , Tuple 50.0 $ pure "F"
         ]
     abcArrGen = sequence $ replicate 200 abcGen
     abcArr = runTestGenFrequency abcArrGen `evalStateT` 0.0 # unwrap
     actual = group' abcArr <#> \(NonEmpty x xs) -> Tuple (length xs + 1) x
-    expected = 
+    expected =
       [ (Tuple 10 "A")
       , (Tuple 20 "B")
       , (Tuple 30 "C")
@@ -61,5 +60,5 @@ check =
       , (Tuple 50 "E")
       , (Tuple 50 "F")
       ]
-  in 
+  in
     assert (expected == actual)
--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -2,15 +2,11 @@ module Test.Main where
 
 import Prelude
 
+import Effect (Effect)
+import Effect.Console (log)
 import Test.Frequency as Frequency
-import Test.Assert (ASSERT)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-type Tests = Eff (console :: CONSOLE, assert :: ASSERT) Unit
-
-main :: Tests
+main :: Effect Unit
 main = do
   log "check frequency"
   Frequency.check
-  
